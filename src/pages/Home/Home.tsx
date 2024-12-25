92983fff5192f8cb2d6823af7dfeddc5d8b865bc
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-12-24 22:34:40 -0500

    Add image upload functionality and preview in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,8 @@ import './Home.css';
 
 export const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
   const navigate = useNavigate();
   const defaultOptions = {
     loop: true,
@@ -17,11 +19,50 @@ export const Home = () => {
     },
   };
 
+  interface FileChangeEvent extends React.ChangeEvent<HTMLInputElement> {
+    target: HTMLInputElement & { files: FileList };
+  }
+
+  const handleFileChange = (event: FileChangeEvent): void => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setPreviewImage(URL.createObjectURL(file));
+    }
+  };
+
   const getSegmentedImages = async () => {
-    setTimeout(() => {
+    if (!selectedFile) {
+      alert('Por favor selecciona una imagen primero.');
       setIsLoading(false);
-      navigate('/result');
-    }, 10000);
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch(
+        'https://trabajo-final-calidad-gps.onrender.com/?multiple=true',
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Archivo subido correctamente:', data);
+        navigate('/result', { state: data });
+      } else {
+        console.error('Error al subir el archivo:', response.statusText);
+        alert('Error al procesar la imagen.');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+      alert('Ocurrió un error al procesar la imagen.');
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -30,8 +71,8 @@ export const Home = () => {
         <span>Segmented</span>
       </div>
       <div className="description">
-        Segmented una aplicación web que permite visualizar la segmentación de
-        caractéres dentro de una imagen utilizando el algoritmo ...
+        Segmented es una aplicación web que permite visualizar la segmentación
+        de caracteres dentro de una imagen utilizando un algoritmo ...
       </div>
       {isLoading ? (
         <>
@@ -47,19 +88,44 @@ export const Home = () => {
         </>
       ) : (
         <>
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
           <div
             className="uploadBox"
-            onClick={() => {
-              setIsLoading(true);
-              getSegmentedImages();
+            style={{
+              backgroundImage: previewImage ? `url(${previewImage})` : 'none',
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundBlendMode: 'screen',
+              backgroundPosition: 'center',
             }}
+            onClick={() => document.getElementById('fileInput')?.click()}
           >
-            <CloudUploadIcon
-              className="uploadIcon"
-              style={{ fontSize: '13rem' }}
-            />
-            <div className="uploadDescription">Sube una imagen</div>
+            {!previewImage && (
+              <>
+                <CloudUploadIcon
+                  className="uploadIcon"
+                  style={{ fontSize: '13rem' }}
+                />
+                <div className="uploadDescription">Sube una imagen</div>
+              </>
+            )}
           </div>
+          {selectedFile && (
+            <button
+              className="uploadButton"
+              onClick={() => {
+                setIsLoading(true);
+                getSegmentedImages();
+              }}
+            >
+              Procesar Imagen
+            </button>
+          )}
         </>
       )}
     </div>
